259e8dde505633dd724ee731a6da5b6a027d504d
Nabeel Sherazi <nabeelsherazi97@gmail.com>
2024-04-14 01:45:00 -0700

    new pelican site :)

---
+++ pelicanconf.py
@@ -0,0 +1,35 @@
+AUTHOR = 'nabeel'
+SITENAME = "nabeel's thoughts"
+SITEURL = ""
+
+PATH = "content"
+
+TIMEZONE = 'America/Los_Angeles'
+
+DEFAULT_LANG = 'en'
+
+THEME = 'themes/pelican-alchemy/alchemy'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (
+    ("about", "https://getpelican.com/"),
+    ("the posts", "https://www.python.org/"),
+    ("you may also like", "https://palletsprojects.com/p/jinja/"),
+)
+
+# Social widget
+SOCIAL = (
+    ("Twitter", "#"),
+)
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+# RELATIVE_URLS = True
